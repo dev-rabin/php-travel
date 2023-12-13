b222e1784c4faaae84e3e374161333be4f4bf9d0
dev-rabin <robinmandhotia@gmail.com>
2023-12-13 21:30:29 +0530

    functions

--- functions.php
+++ functions.php
@@ -1,7 +1,18 @@
 <?php
 
-function myMessage(){
-    echo"Namaste Dunia";
+// function myMessage(){
+//     echo"Namaste Dunia";
+// }
+// myMessage();
+
+function schoolMarks($myArray){
+
+    foreach ($myArray as $value) {
+        $sum += $value;
+    }
+    return $sum;
 }
-myMessage();
+$robin =  [92,65,63,78,98];
+$sumMarks = schoolMarks($robin);
+echo "Total Marks Obtained by Robin out of 500 = ". $sumMarks; 
 ?>
